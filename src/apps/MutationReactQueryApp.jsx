e3b6039b481931a2598c94d1134632b811f43fc7
devrathrana <devrath.rana@bold.com>
2025-04-26 22:51:11 +0530

    mutation.mutate() in react query

--- src/apps/MutationReactQueryApp.jsx
+++ src/apps/MutationReactQueryApp.jsx
@@ -1,9 +1,10 @@
 import { useMutation } from "@tanstack/react-query";
 import React from "react";
 
-const time = (duration, isError = false) => {
+const time = (duration, isError = false, params) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
+      console.log("params", params);
       isError ? reject("Promise rejected") : resolve("Promise resolved");
       console.log("Promise was run!!");
     }, duration);
@@ -12,7 +13,7 @@ const time = (duration, isError = false) => {
 
 const MutationReactQueryApp = () => {
   const mutation = useMutation({
-    mutationFn: () => time(1000),
+    mutationFn: (params) => time(1000, false, params),
     onSuccess: (data) => {
       // It can be successful with setQueryData where you can update UI based on post request response by mutating the cache
       console.log("Success", data);
@@ -23,7 +24,19 @@ const MutationReactQueryApp = () => {
 
   const callMutate = async () => {
     console.log("Updating post...");
-    await mutation.mutateAsync();
+    await mutation.mutateAsync("Param from mutateAsync");
+    console.log("Post updated");
+  };
+
+  const callMutateNotAsync = () => {
+    console.log("Updating post...");
+    mutation.mutate("Param from mutate", {
+      onSuccess: (data) => {
+        console.log("Success from mutate", data);
+      },
+      onError: (err) => console.log("Error from mutate", err),
+      onSettled: (data, err) => console.log("Settled from mutate", data, err),
+    }); //this will not return a promise
     console.log("Post updated");
   };
 
@@ -31,6 +44,7 @@ const MutationReactQueryApp = () => {
     <div>
       MutationReactQueryApp
       <button onClick={callMutate}>Submit form</button>
+      <button onClick={callMutateNotAsync}>Submit form not async</button>
     </div>
   );
 };
